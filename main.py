45e95cc63d39fa2956e96697742625f728de3878
unknown <asadullah.dal17@gmail.com>
2024-08-17 08:36:32 +0500

    add required social media and applyed styling to it

--- main.py
+++ main.py
@@ -38,11 +38,20 @@ html_code = """
                 <h2>Asadullah Dal</h2>
                 <p>Educator, YouTuber, Freelancer</p>
                 <p>Computer Vision & Robotics Specialist</p>
-                <div class="social-media">
-                    <a href="https://twitter.com/yourprofile" target="_blank"><i class="fab fa-twitter"></i></a>
-                    <a href="https://github.com/yourprofile" target="_blank"><i class="fab fa-github"></i></a>
-                    <a href="https://linkedin.com/in/yourprofile" target="_blank"><i class="fab fa-linkedin"></i></a>
-                </div>
+            <div class="social-media">
+            <a href="https://www.youtube.com/c/aiphile" target="_blank" title="YouTube">
+                <i class="fab fa-youtube"></i>
+            </a>
+            <a href="https://www.linkedin.com/in/asadullah-dal/" target="_blank" title="LinkedIn">
+                <i class="fab fa-linkedin"></i>
+            </a>
+            <a href="https://github.com/asadullah-dal17" target="_blank" title="GitHub">
+                <i class="fab fa-github"></i>
+            </a>
+            <a href="https://www.instagram.com/aiphile" target="_blank" title="Instagram">
+                <i class="fab fa-instagram"></i>
+            </a>
+            </div>
             </div>
         </div>
         <div class="introduction">
